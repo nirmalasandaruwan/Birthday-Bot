b465a0f6fb08d30a6452a4414ed0cf2ec493931b
Nirmala Sandaruwan <nirmalaekanayake222@gmail.com>
2026-08-13 19:26:42 +0530

    Update font to myfont.ttf

--- main.py
+++ main.py
@@ -76,18 +76,12 @@ def create_birthday_post():
                 # 5. අලුත් නම ටයිප් කිරීම
                 text_to_add = f"{kid['name'].upper()}"
                 
-                try:
-                    font = ImageFont.truetype("arial.ttf", 180) 
-                except IOError:
-                    font = ImageFont.load_default()
+                # 5. අලුත් නම ටයිප් කිරීම
+                text_to_add = f"{kid['name'].upper()}"
+                
+                font = ImageFont.truetype("myfont.ttf", 180) 
 
                 text_bbox = draw.textbbox((0, 0), text_to_add, font=font)
-                text_width = text_bbox[2] - text_bbox[0]
-                
-                x_position = (3375 - text_width) / 2
-                y_position = 2380 
-
-                draw.text((x_position, y_position), text_to_add, fill="#2a2a2a", font=font)
 
                 # 6. අවසාන පෝස්ට් එක සේව් කරනවා
                 final_image = template.convert("RGB")
